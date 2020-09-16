ebd0796cd6d917927cde64fdb186e48378923337
Lucas Han <ldhan0715@gmail.com>
2020-09-16 19:45:53 +0900

    Update: Docs

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Dikipedia
 
-Clone of [Wikipedia](https://en.wikipedia.org), made in order to practice Node.js & Express.
+### Clone of [Wikipedia](https://en.wikipedia.org), made in order to practice Node.js & Express.
+
+## Module/APIs
+
+- Express
+  - Body Parser
+  - Cookie Parser
+  - Compression
+- MySQL
+- Mailgun
--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(require('compression')());
 app.use(require('cookie-parser')());
 app.use(express.static('public'));
 app.use('/account',require('./lib/account'));
+app.use('/docs',require('./lib/docs'));
 
 app.get('/',(req,res) => {
     fs.readFile('html/index.html','utf8',(err,data) => {
@@ -22,17 +23,35 @@ app.get('/',(req,res) => {
 
 app.get('/search',(req,res) => {
     const query = url.parse(req.url,true).query.q;
-    db.query('select * from docs where title = ?',[query],(err,doc) => {
-        if (!doc[0]) {
-            let acc = template.login;
-            if (req.cookies.un) acc = template.accPage;
+    let searchResults = new Array();
+    db.query('select * from docs',(err,docs) => {
+        docs.forEach((doc) => {
+            template.parse(doc.keywords).forEach((keyword) => {
+                if (keyword === query) searchResults.push([doc.id,doc.title,doc.content]);
+            });
+        });
+        let acc = template.login;
+        if (req.cookies.un) acc = template.accPage;
+        if (!searchResults[0]) {
             res.send(template.html(query,acc,template.part('search',query)));
+        } else {
+            if (searchResults.length > 1) {
+                res.send(template.html(query,acc,`<div class='content'><h1>Search results for '${query}'</h1>${template.listData(searchResults)}</div>`));
+            } else {
+                res.redirect(`/docs/${searchResults[0][0]}`);
+            }
         }
     });
 });
+
+app.get('/contrib',(req,res) => {
+    let acc = template.login;
+    if (req.cookies.un) acc = template.accPage;
+    res.send(template.html('Contributors',acc,template.part('contrib','')));
+})
 
 app.use((req,res) => {
     res.send(template.notFound);
 });
 
-app.listen(3000);
+app.listen(80);
