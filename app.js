74cdf3391fddae8ee6d699ed7e9df05489814762
Lucas Han <ldhan0715@gmail.com>
2020-09-20 12:23:54 +0900

    Update: Cookie to session

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ const host = fs.readFileSync('profile.txt','utf8').split(',')[2];
 app.use(require('body-parser').urlencoded({extended: false}));
 app.use(require('compression')());
 app.use(require('cookie-parser')());
+require('./lib/session')(app);
 app.use(express.static('public'));
 app.use('/account',require('./lib/account'));
 app.use('/docs',require('./lib/docs'));
 
 app.get('/',(req,res) => {
     fs.readFile('html/index.html','utf8',(err,data) => {
-        res.send(data.replace('$1',template.accLink(req.cookies.un)));
+        res.send(data.replace('$1',template.accLink(req.session.un)));
     });
 });
 
@@ -30,10 +31,10 @@ app.get('/search',(req,res) => {
             });
         });
         if (!searchResults[0]) {
-            res.send(template.html(query,template.accLink(req.cookies.un),template.part('noResult',[query])));
+            res.send(template.html(query,template.accLink(req.session.un),template.part('noResult',[query])));
         } else {
             if (searchResults.length > 1) {
-                res.send(template.html(query,template.accLink(req.cookies.un),`<div class='content'><h1>Search results for '${query}'</h1>${template.listData(searchResults)}</div>`));
+                res.send(template.html(query,template.accLink(req.session.un),`<div class='content'><h1>Search results for '${query}'</h1>${template.listData(searchResults)}</div>`));
             } else {
                 res.redirect(`/docs/${searchResults[0][0]}`);
             }
@@ -42,15 +43,15 @@ app.get('/search',(req,res) => {
 });
 
 app.get('/contributors',(req,res) => {
-    res.send(template.html('Contributors',template.accLink(req.cookies.un),template.part('contrib')));
+    res.send(template.html('Contributors',template.accLink(req.session.un),template.part('contrib')));
 });
 
 app.get('/contact',(req,res) => {
-    res.send(template.html('Contact',template.accLink(req.cookies.un),template.part('contact')));
+    res.send(template.html('Contact',template.accLink(req.session.un),template.part('contact')));
 });
 
 app.use((req,res) => {
-    res.send(template.html('Not Found',template.accLink(req.cookies.un),template.part('notFound')));
+    res.send(template.html('Not Found',template.accLink(req.session.un),template.part('notFound')));
 });
 
 app.listen(80);
